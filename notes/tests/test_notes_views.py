50c4f7958a6ed4ef894edf42998a6ad228305f73
Ashok Rai <ashokrai2053@gmail.com>
2022-07-19 17:43:40 +0545

    Add Test for post method for note createview

--- notes/tests/test_notes_views.py
+++ notes/tests/test_notes_views.py
@@ -53,3 +53,16 @@ def test_list_endpoint_only_notes_from_aunthenticated_user(client, logged_user):
     content = str(response.content)
     assert 'New Test Note' not in content
     assert 2 == content.count('<h3>')
+    
+
+@pytest.mark.django_db
+def test_create_endpoint_receives_form_data(client, logged_user):
+  form_data = {'title': 'An impressive title', 'text': 'A really interesting text'}
+  
+  response = client.post(path='/smart/notes/new', data=form_data, follow=True)
+  
+  assert 200 == response.status_code
+  assert 'notes/notes_list.html' in response.template_name
+  assert 1 == logged_user.notes.count()
+  assert 'An impressive title' == logged_user.notes.first().title
+  
